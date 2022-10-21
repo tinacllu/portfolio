47af547f784432b704e0c3a4c58bb681b74e5251
tinacllu <tina.lu0107@gmail.com>
2022-10-21 16:18:10 -0400

    added loading animation, bio, and set up contact form

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+window.addEventListener('load', () => {
+    const loadingScreen = document.querySelector(".loadingScreen");
+    loadingScreen.classList.add("disappear");
+});
+
 //hamburger menu
 const navigation = document.querySelector(".navigation");
 const exit = document.querySelector("#x");
 
 exit.addEventListener("click", () => {
-    navigation.style.display = "none";
+    navigation.classList.remove("slideIn");
+    navigation.classList.add("slideOut");
 })
 
 const menu = document.querySelector(".menu");
 
 menu.addEventListener("click", () => {
-    navigation.style.display = "block";
+    navigation.classList.add("slideIn");
+    navigation.classList.remove("slideOut");
 });
+
+
+
