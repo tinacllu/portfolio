2f5fc0cf1ec59e43c3effd1cf19ceeacba0ff4d3
tinacllu <tina.lu0107@gmail.com>
2022-10-24 20:03:59 -0400

    prevented page redirect after contact form submission

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ menu.addEventListener("click", () => {
 const navLinks = document.querySelectorAll(".navLinks");
 
 navLinks.forEach((navLink) => {
-    console.log(navLink);
     navLink.addEventListener("click", () => {
     console.log("hi")
     navigation.classList.remove("slideIn");
@@ -35,13 +34,33 @@ navLinks.forEach((navLink) => {
 });
 
 // form submission
-const form = document.querySelector("form");
-form.addEventListener("submit", (event) => {
+const form = document.getElementById("contactForm");
+
+async function handleSubmit(event) {
     event.preventDefault();
-    form.submit();
-    form.reset();
-    return false;
-})
+    const status = document.getElementById("status");
+    const data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            "Accept": "application/json"
+        }
+    }).then(response => {
+        if (response.ok) {
+            status.classList.add("success");
+            form.reset();
+            status.innerHTML = `<p>Thanks for your message!</p>`;
+        } else {
+            throw new Error(response);
+        }
+    }).catch(error => {
+        status.classList.add("error");
+        status.innerHTML = `<p>Oh no - something went wrong! Please try again later, or send me an <a href="mailto:tina.lu0107@gmail.com">email</a>.</p>`
+    });
+}
+
+form.addEventListener("submit", handleSubmit);
 
 
 console.log(window.scrollY);
